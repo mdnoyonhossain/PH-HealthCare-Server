f0441e63d175bf577850ebd8b2c62c8feb2b908a
Noyon Hossain <mdshahinurrahmannayon@gmal.com>
2024-04-19 09:35:54 +0600

    Handling UncaughtException And UnhandledRejection Errors

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,26 @@ import config from "./config";
 async function main() {
     const server: Server = app.listen(config.port, () => {
         console.log(`PH Health Care Server is Running on port: ${config.port}`);
-    })
+    });
 
+    const exitHandler = () => {
+        if (server) {
+            server.close(() => {
+                console.info("Server Closed!");
+            });
+        }
+        process.exit(1);
+    }
+
+    process.on('uncaughtException', (error) => {
+        console.log(error);
+        exitHandler();
+    });
+
+    process.on('unhandledRejection', (error) => {
+        console.log(error);
+        exitHandler();
+    });
 }
 
 main();
